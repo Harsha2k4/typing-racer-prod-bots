4570d528a105e431a7f9db1752e38bd71aa110cb
Harsha2k4 <110480991+Harsha2k4@users.noreply.github.com>
2025-08-28 23:45:18 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE = import.meta.env.VITE_API_BASE_URL|| 'http://127.0.0.1:8000'
+const BASE = import.meta.env.VITE_API|| 'http://127.0.0.1:8000'
 
 
 export const api = {
